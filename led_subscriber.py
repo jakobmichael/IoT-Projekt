a1297298e42c806be0e3cac631b04d1c468ab0ed
jakob_michael <jakob_luge@mailbox.org>
2022-04-29 08:53:34 +0200

    Update ID management in database, add database for rfid uids

--- led_subscriber.py
+++ led_subscriber.py
@@ -3,17 +3,17 @@ import time
 from datetime import datetime
 import sqlite3 as sql
 
-PROTOCOL_DATABASE = "ACCESS_PROTOCOL" 
-
-database_connection = sql.connect('access_protocol.db',check_same_thread=False)
+database_connection = sql.connect('iot_project.db',check_same_thread=False)
 db_cursor = database_connection.cursor()
 db_cursor.execute("DROP TABLE ACCESS_PROTOCOL")
 
 database_connection.commit()
 
-access_id = 1
 
-sql_insertion_query = ''' INSERT INTO ACCESS_PROTOCOL(access_id,valid,access_time)
+sql_access_insertion_query = ''' INSERT INTO ACCESS_PROTOCOL(valid,access_time)
+              VALUES(?,?,?) '''
+              
+sql_uid_insertion_query = ''' INSERT INTO VALID_UIDS(rfid_uid,holder)
               VALUES(?,?,?) '''
 
 
@@ -28,19 +28,36 @@ def create_access_table(connection):
             );
         """)
         
+
+def create_valid_uid_table(connection):
+    with connection:
+        connection.execute("""
+            CREATE TABLE VALID_UIDS (
+                uid_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                rfid_uid TEXT,
+                holder DATETIME
+            );
+        """)
+        
         
 def query_table(connection):
     with connection:
         data = connection.execute("SELECT * FROM ACCESS_PROTOCOL")
-        print(data)
         for row in data:
             print(row)
-            
-
-def insert_access_into_database(access_id):
+     
+     
+def insert_into_uid_database(uid,holder):
     dt = datetime.now()
     with database_connection:
-        database_connection.execute(sql_insertion_query, (access_id,"valid", dt))
+        database_connection.execute(sql_access_insertion_query, (uid, holder))
+    
+    database_connection.commit()       
+
+def insert_access_into_database():
+    dt = datetime.now()
+    with database_connection:
+        database_connection.execute(sql_access_insertion_query, ("valid", dt))
     
     database_connection.commit()
 
@@ -49,20 +66,16 @@ def on_connect(client, userdata, flags, rc):
     print("Connecting to broker")
   
 def on_message(client, userdata, message):
-    global access_id
     
     message_as_string = message.payload.decode('utf-8')
     print("Message received: "  + message_as_string)
     
-    insert_access_into_database(access_id)
+    insert_access_into_database()
     
-    access_id += 1
     
     
 
-  
 
-  
 MQTT_ADDRESS= input("Please specify broker IP address: ")               
 
 MQTT_TOPIC = "motion"
